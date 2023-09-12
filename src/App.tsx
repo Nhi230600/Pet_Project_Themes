764861f09e8d3e22945deb985068f94f470bfc03
TruongYNhi <20521720@gm.uit.edu.vn>
2023-09-12 09:14:18 +0700

    update-1209

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import Router from "../src/routes/Routes";
-import FooterDashboard from "./components/FooterDashboard";
-import HeaderDashboard from "./components/HeaderDashboard";
-import { toast, ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Routes from "../src/routes/Routes";
+import HeaderDashboard from "./components/HeaderDashboard";
 
 import "react-toastify/dist/ReactToastify.css";
 
 import { Footer, Header } from "./components";
-import Homepage from "./screens/Homepage/Homepage";
 
 const App = () => {
   return (
