da01c34d52ef9ccbe4a32962ec51862e2ff2b76b
viet <viethxse@gmail.com>
2023-09-14 09:54:48 +0700

    component admin

--- src/screens/Employee/ListEmployee/Content.tsx
+++ src/screens/Employee/ListEmployee/Content.tsx
@@ -5,7 +5,6 @@ import ListEmployee from "./Page";
 const App = () => {
   return (
     <div>
-      {/* Sử dụng AdminInterface và truyền ContentDashboard vào prop content */}
       <AdminContent content={<ListEmployee />} />
     </div>
   );
