a740c2a642a5d7fd3855e84d60b651a813f14298
CongLuong0201 <congluong9a3@gmail.com>
2023-09-22 16:42:31 +0700

    update-service

--- src/components/Service/ServiceCard.tsx
+++ src/components/Service/ServiceCard.tsx
@@ -3,8 +3,10 @@ import { service } from "./serviceData";
 const ServiceCard = ({ service }: { service: service }) => {
   const { src, title, main } = service;
   return (
-    <div className="bg-yellow-100 rounded-md shadow-md shawdow-yellow-100 relative ">
-      <img src={src} className="w-full " />
+    <div className="bg-yellow-100 rounded-md shadow-md shawdow-yellow-100 relative   ">
+      <div>
+        <img src={src} className="w h-3/6" />
+      </div>
       <div>
         <h4>{title}</h4>
         <p>{main}</p>
--- src/components/TrainerList/index.tsx
+++ src/components/TrainerList/index.tsx
@@ -1,56 +1,56 @@
-import React from 'react'
-import './TrainerList.css'
-import trainer1 from 'images/trainer1.jpg'
-import trainer2 from 'images/trainer2.jpg'
-import trainer3 from 'images/trainer3.jpg'
-import trainer4 from 'images/trainer4.jpg'
+import React from "react";
+import "./TrainerList.css";
+import Button from "components/Button/Index";
+import trainer1 from "images/trainer1.jpg";
+import trainer2 from "images/trainer2.jpg";
+import trainer3 from "images/trainer3.jpg";
+import trainer4 from "images/trainer4.jpg";
 
 const TrainerList = () => {
   const trainerdata = [
     {
-      img : trainer4,
-      name: 'John Nguyen',
-      position: 'Trainer',
-      exp: '4 years' 
+      img: trainer4,
+      name: "John Nguyen",
+      position: "Trainer",
+      exp: "4 years",
     },
     {
-      img : trainer2,
-      name: 'Malina Tran',
-      position: 'Veterinarian',
-      exp: '3 years' 
+      img: trainer2,
+      name: "Malina Tran",
+      position: "Veterinarian",
+      exp: "3 years",
     },
     {
-      img : trainer3,
-      name: 'Jack Ngo',
-      position: 'Founder',
-      exp: '5 years' 
-    }
-  ]
-  
+      img: trainer3,
+      name: "Jack Ngo",
+      position: "Founder",
+      exp: "5 years",
+    },
+  ];
+
   return (
-    <div className='trainerlist-container'>
+    <div className="trainerlist-container">
       <div className="trainerlist-container-title">
         <span>Our Trainer</span>
       </div>
-      
+
       <div className="trainerlist-container-content">
         {trainerdata.map((data) => (
           <div className="trainerlist-container-content-detail">
             <div className="trainerlist-container-content-detail-img">
-              <img className='trainer-img' src={data.img} alt="" />
+              <img className="trainer-img" src={data.img} alt="" />
             </div>
             <div className="trainerlist-container-content-detail-name">
               <h1>{data.name}</h1>
               <p>{data.position}</p>
               <p>{data.exp}</p>
-              <button className='trainerlist-container-content-detail-btn'>VIEW MORE</button>
+              <Button />
             </div>
           </div>
-        ))
-        }
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TrainerList
+export default TrainerList;
