7ddf7a15b162a83874e25a30347ed51e5f1ff19c
Nguyen Hong Ngoc <134280054+pby1905@users.noreply.github.com>
2023-09-25 15:03:33 +0700

    m

--- src/components/Service/index.tsx
+++ src/components/Service/index.tsx
@@ -6,15 +6,11 @@ import { serviceData } from "./serviceData";
 const Service = () => {
   return (
     <>
-      <div className="listservice-container">
-        <div className="listservice-container-title">
-          <span className="listservice-container-title-1">List Service</span>
-        </div>
-        <div className="grid grid-cols-3 gap-10 mb-10">
-          {serviceData.map((service, index) => (
-            <ServiceCard key={index} service={service} />
-          ))}
-        </div>
+      <h3 className="border-b mt-12 mb-12 pb-6">List Service</h3>
+      <div className="grid grid-cols-3 gap-10 mb-10">
+        {serviceData.map((service, index) => (
+          <ServiceCard key={index} service={service} />
+        ))}
       </div>
     </>
   );
