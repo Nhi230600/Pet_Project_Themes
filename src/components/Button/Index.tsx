4167f78034f9d1569ca82b5b759b267b323c8802
quangttm1 <minhquangbeomap@gmail.com>
2023-09-25 09:38:49 +0700

    update-trainerlist-btn

--- src/components/Button/Index.tsx
+++ src/components/Button/Index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Button.css";
 
-const Button = () => {
+type btnProps = {
+  name_btn: string;
+  width_btn : string;
+  height_btn : string; 
+}
+
+const Button = (props : btnProps) => {
+  const {name_btn, width_btn, height_btn} = props;
   return (
     <div>
-      <button className="trainerlist-container-content-detail-btn">
-        VIEW MORE
+      <button className={"trainerlist-container-content-detail-btn "+"w-"+ width_btn + " h-" + height_btn} >
+        {name_btn}
       </button>
     </div>
   );
